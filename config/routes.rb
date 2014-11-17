c51e043dedd5540cdc6e828fc81638c90afed367
agugar <fjagugar@gmail.com>
2014-11-17 16:12:45 +0100

    Profile page added
    Profile page have been added. No tests made for this page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ FirstApp::Application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
 
+########### Users #############
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
